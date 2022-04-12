b3eb7547683237ea650c30e5445efd8127442b34
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-04-12 10:59:44 -0500

    Creación de proyecto

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+$hostname     = "localhost"; // Enter Your Host Name
+$username     = "root";      // Enter Your Table username
+$password     = "";          // Enter Your Table Password
+$databasename = "newplast_web"; // Enter Your database Name
+
+
+$conn = new mysqli($hostname, $username, $password, $databasename);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
