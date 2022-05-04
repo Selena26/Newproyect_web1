5bfb5407aaf656e9879b24ee4938cd1bab66cf78
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-05-04 16:59:05 -0500

    Depuracion

--- Pry_newplast/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-    session_start();
-    header("location: index.php");
-    session_destroy();
-
-?>
--- settings/text-color.php
+++
@@ -1,9 +0,0 @@
-<br></br>
-
-<div class="content-box">
-	<div class="row">
-		<div class="col">
-			<h4>Change Text Color</h4>
-		</div>
-	</div>
-</div>
