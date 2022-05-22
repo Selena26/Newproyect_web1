25ac83d3c8d3c4db6a5e39529076e6b8da431210
Hector858 <hicedeno@espe.edu.ec>
2022-05-22 18:09:22 -0500

    incorporación del mapa y la opción de cerrar sesión

--- Clase_Dashboard_PHPMySQL/sistema/backend_puntoacopio.php
+++ Clase_Dashboard_PHPMySQL/sistema/backend_puntoacopio.php
@@ -88,6 +88,26 @@ if(empty($_GET['id']) && !empty($_GET['name']) && $_GET['name']=='punto_acopio')
    
 }
 
+// ======= delete data from database ============//
+if(!empty($_GET['deleteId']) && !empty($_GET['deleteData']))
+{
+
+   $id= legal_input($_GET['deleteId']);
+   $deleteData=legal_input($_GET['deleteData']);
+   $tableName= $deleteData;
+  
+   $deleteData=delete_data($tableName, $id);
+ 
+    if($deleteData){
+      echo "<span class='success'>".$tableName." data was deleted</span>";
+    }else{
+      echo  "<span class='fail'>Error...Check your query</span>";
+    }
+   
+}
+
+// MySQL Query for database operation 
+
 function update_data($data, $tableName, $id){
 
   global $db;
@@ -112,22 +132,107 @@ function update_data($data, $tableName, $id){
 
 }
 
-function legal_input($value) {
-  $value = trim($value);
-  $value = stripslashes($value);
-  $value = htmlspecialchars($value);
-  return $value;
-}
+
 
 function delete_data($tableName, $id){
-  global $db;
+global $db;
 
-  $query="DELETE FROM ".$tableName." WHERE id=".$id;
-  $result= $db->query($query);
-  if($result){
-     return true;
-  }else{
-     echo "Error found in ".$db->error;
-  }
+$query="DELETE FROM ".$tableName." WHERE id=".$id;
+$result= $db->query($query);
+if($result){
+  return true;
+}else{
+  echo "Error found in ".$db->error;
+}
 
 }
+function delete_menu($tableName, $id, $menu){
+global $db;
+if($menu=='header_menu')
+{
+ $query="DELETE FROM ".$tableName." WHERE id=$id AND header_menu=1";
+
+}else if($menu=='footer_menu'){
+$query="DELETE FROM ".$tableName." WHERE id=$id AND footer_menu=1";
+}
+
+$result= $db->query($query);
+if($result){
+  return true;
+}else{
+  echo "Error found in ".$db->error;
+}
+
+}
+// convert illegal input value to ligal value formate
+function legal_input($value) {
+$value = trim($value);
+$value = stripslashes($value);
+$value = htmlspecialchars($value);
+return $value;
+}
+
+
+function insert_data(array $data, string $tableName){
+    
+  global $db;
+
+  $tableColumns = $userValues = ''; 
+  $num = 0; 
+  foreach($data as $column=>$value){ 
+       $comma = ($num > 0)?', ':''; 
+       $tableColumns .= $comma.$column; 
+       $userValues  .= $comma."'".$value."'"; 
+       $num++; 
+   } 
+ $insertQuery="INSERT INTO ".$tableName."  (".$tableColumns.") VALUES (".$userValues.")";
+ $insertResult=$db->query($insertQuery);
+ if($insertResult){
+    return true;
+ }else{
+    return "Error: " . $insertQuery . "<br>" . $db->error;
+ }
+
+}
+
+
+
+function check_unique_content($tableName){
+
+global $db;
+
+
+    $query="SELECT * FROM ".$tableName;
+   $result= $db->query($query);
+    $totalRows= $result->num_rows;
+   return $totalRows;
+   
+
+
+
+
+}
+function check_unique_menu($tableName, $menuName){
+
+global $db;
+
+
+   $query="SELECT * FROM ".$tableName." WHERE menu_name='".$menuName."'";
+
+    $result= $db->query($query);
+    if($result)
+    {
+    $totalRows= $result->num_rows;
+    return $totalRows;
+   }else{
+   return $totalRows=0;
+  }
+
+    return $db->error;
+  
+
+
+
+
+}
+?>
--- Clase_Dashboard_PHPMySQL/sistema/partials/header.php
+++ Clase_Dashboard_PHPMySQL/sistema/partials/header.php
@@ -1,3 +1,18 @@
+<?php
+	
+	session_start();
+	
+	if(!isset($_SESSION['id'])){
+		header("Location: index.php");
+	}
+	
+	$nombre = $_SESSION['nombre'];
+	$tipo_usuario = $_SESSION['tipo_usuario'];
+	
+	
+?>
+
+
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <a class="navbar-brand" href="principal.php">New Plast</a><button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button
     ><!-- Navbar Search-->
@@ -12,11 +27,11 @@
     <!-- Navbar-->
 <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><!--<?php echo $nombre; ?>--><i class="fas fa-user fa-fw"></i></a>
+            <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $nombre; ?><i class="fas fa-user fa-fw"></i></a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#">Configuración</a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="./logout.php">Salir</a>
+                <a class="dropdown-item" href="logout.php">Salir</a>
             </div>
         </li>
     </ul>
@@ -26,4 +41,12 @@
     <button value="1" >Sí</button><button value="0">No</button>
 </div>
 <div id="alertBox">mhvmbvbm</div>
-<link href="css/styles.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="assets/demo/chart-area-demo.js"></script>
+<script src="assets/demo/chart-bar-demo.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="assets/demo/datatables-demo.js"></script>
