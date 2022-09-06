330134d76501670e613a92e8c063949a397538a2
Hector858 <hicedeno@espe.edu.ec>
2022-09-06 09:41:41 -0500

    cambio de titulo

--- Clase_Dashboard_PHPMySQL/sistema/ver_marcabotellas.php
+++ Clase_Dashboard_PHPMySQL/sistema/ver_marcabotellas.php
@@ -45,7 +45,7 @@
 			<div id="layoutSidenav_content">
 				<main>
 					<div class="container-fluid">
-                    <h1 class="mt-4">Registro de Botellas</h1>
+                    <h1 class="mt-4">Registro de Marca de Botellas</h1>
 						<ol class="breadcrumb mb-4">
 							<li class="breadcrumb-item"><a href="marca_botellas.php">Registro de marca de botellas</a></li>
 							<li class="breadcrumb-item active">Ver Registro</li>
